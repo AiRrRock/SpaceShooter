bffc7b6fdbcf8c4ef9c19caf17c717356b139d81
Airt <alex.borichev@gmail.com>
2021-02-07 22:12:02 +0300

    Added movement to position

--- core/src/com/borichev/SpaceShooter.java
+++ core/src/com/borichev/SpaceShooter.java
@@ -1,43 +1,13 @@
 package com.borichev;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.Game;
+import com.borichev.screen.MenuScreen;
 
-public class SpaceShooter extends ApplicationAdapter {
-    SpriteBatch batch;
-    Texture img;
-    Texture background;
-    TextureRegion region;
-    int x;
+public class SpaceShooter extends Game {
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        img = new Texture("badlogic.jpg");
-        background =new Texture("nebula.jpg");
-        region = new TextureRegion(img, 128, 256);
-        x = 0;
+        setScreen(new MenuScreen());
     }
 
-    @Override
-    public void render() {
-        Gdx.gl.glClearColor(0.5f, 0.6f, 0.4f, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        batch.draw(background, 0,0, 1080,1920);
-        batch.draw(img, x, x);
-        batch.draw(region, 0, 0);
-        batch.end();
-        x++;
-    }
-
-    @Override
-    public void dispose() {
-        batch.dispose();
-        img.dispose();
-    }
 }
